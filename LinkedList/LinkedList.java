03f5f1e888dcbdd65baec3e1eef937e25ef45fce
pc95184763 <pc95184763@qq.com>
2021-07-29 21:48:21 +0800

    July 29, 2021

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class LinkedList {
+
+  
+
+  public ListNode reverse(ListNode head) {
+    // Write your solution here
+    if (head == null ) {
+      return null ;
+    }
+    if (head.next == null ) {
+      return head ;
+    }
+    //   1 --> 2  --> 4 -->   7
+    //   1 <-- 2 <-- 4 <-- 7
+    ListNode nxt = head.next ;
+    ListNode newHead = reverse(head.next) ;
+    nxt.next = head ;
+    head.next = null ;
+
+    return newHead ;
+  }
+}
+
+class ListNode {
+  public int value;
+     public ListNode next;
+    public ListNode(int value) {
+      this.value = value;
+      next = null;
+    }
+  }
