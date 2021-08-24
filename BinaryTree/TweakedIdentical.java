d32ab5b9c2735262dfd9676097a24d969ec1d111
pc95184763 <pc95184763@qq.com>
2021-08-24 22:33:19 +0800

    Binary Tree

---
+++ BinaryTree/TweakedIdentical.java
@@ -0,0 +1,18 @@
+package BinaryTree;
+
+public class TweakedIdentical {
+
+  // T：O(n ^ 2)
+  // S: O(height)
+  public boolean isTweakedIdentical(TreeNode one, TreeNode two) {
+    // Write your solution here
+    if ( one == null && two == null) {
+      return true ;
+    }
+    if ( one == null || two == null || one.key != two.key) {
+      return false ;
+    }
+    return isTweakedIdentical(one.left, two.left) && isTweakedIdentical(one.right, two.right) ||
+        isTweakedIdentical(one.right, two.left) && isTweakedIdentical(one.left, two.right) ;
+  }
+}
